5dc62ae0595895805a38ddadf3484b2195520199
Brundha V <brundha.velusamy@syncfusion.com>
2026-09-23 11:46:38 +0530

    Validated switch enabled UI selector

--- Samples/QuickGridUrlNavigationSampleNET10To11/Program.cs
+++ Samples/QuickGridUrlNavigationSampleNET10To11/Program.cs
@@ -1,5 +1,9 @@
 using QuickGridUrlNavigationSampleNET10To11.Components;
 
+AppContext.SetSwitch(
+"Microsoft.AspNetCore.Components.QuickGrid.EnableUrlBasedQuickGridNavigationAndSorting",
+false);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
